3d268a77acd96fdd67225b086fe434c23b52fbc9
LeonardoViotti <leonardoviotti@gmail.com>
2020-03-03 16:01:49 -0500

    Last version of initial draft code.

--- cv.py
+++ cv.py
@@ -55,4 +55,4 @@ bbox, label, conf = cv.detect_common_objects(im)
 output_image = draw_bbox(im, bbox, label, conf)
 plt.imshow(output_image)
 plt.show()
-print('Number of cars in the image is '+ str(label.count('car')))`
+print('Number of cars in the image is '+ str(label.count('car')))
